723796a74b53a4a1503ccf917b016845ea6f8d98
hhhhhty <zhangty2@shanghaitech.edu.cn>
2021-01-03 19:59:46 +0800

    Update Logic_Inference

--- code/MineSweeper.py
+++ code/MineSweeper.py
@@ -3,8 +3,11 @@ MineSweeper Game API.
 You can play the game in terminal.
 e.g. type 3, 0 to uncover the tile in Row_3, Col_0.
 """
+LENGTH = 6
+WIDTH = 6
+NMINES = 7
 
-
+MINE = -1
 import numpy as np
 
 
@@ -12,10 +15,10 @@ import numpy as np
 class MineSweeper:
     def __init__(self):
         # params
-        self.dim1 = 6
-        self.dim2 = 6
+        self.dim1 = LENGTH
+        self.dim2 = WIDTH
         self.totalCells = self.dim1 * self.dim2
-        self.nMines = 7
+        self.nMines = NMINES
         self.mines = np.zeros([self.dim1, self.dim2])
         self.neighbors = np.zeros([self.dim1, self.dim2])
         self.state = np.zeros([self.dim1, self.dim2])
@@ -77,6 +80,105 @@ class MineSweeper:
             if np.sum(np.isnan(self.state)) == self.nMines:
                 self.gameOver = True
                 self.victory = True
+
+class Logic_inference:
+      def __init__(self, state):
+        self.neighborSet = {}
+        self.dim1 = LENGTH
+        self.dim2 = WIDTH
+        self.state = state
+        self.edgeDict = dict()
+        self.mines = list()
+      
+      # Update the state of the game  
+      def stateUpdate(self, states):
+        self.state = states
+        for mine in self.mines:
+          assert(np.isnan(self.state[mine[0], mine[1]]))
+          self.state[mine[0], mine[1]] = MINE
+
+      # Input a coordinate return a set contains (x,y) of the neighbors
+      def getNeighbors(self, coordinate):
+        x = coordinate [0]
+        y = coordinate [1]
+        neighbors = []
+        for i in range(-1, 2):
+          if 0 <= x + i < self.dim1: 
+            for j in range(-1, 2):
+              if 0 <= y + j < self.dim2 and (i * j + i + j) != 0:
+                neighbors.append((x + i, y + j))
+        return neighbors
+      
+
+      def logicInference(self):
+          
+        
+        
+          
+
+
+      # Update the self.edgeDict 
+      def edgeDictUpdate(self):
+        self.edgeDict = dict()
+        for x in range(self.dim1):
+          for y in range(self.dim2):
+            if np.isnan(self.state[x,y]):
+              continue
+            neighbors = self.getNeighbors(x,y)
+            nanSet = list()
+            for nb in neighbors:
+              if np.isnan(self.state[nb[0], nb[1]]):
+                nanSet.append(nb)              
+            if len(nanSet) != 0:
+              self.edgeDict.update({(x, y): nanSet})
+
+      def mineUpdate(self,coordinate):
+        self.mines.append(coordinate)
+        assert(np.isnan(self.state[coordinate[0], coordinate[1]]))
+        self.state[coordinate[0], coordinate[1]] = MINE
+
+      # Search the edgeDict to find the # uncover neighbors equals to the cell 
+      def basicRule1(self):
+        for key in self.edgeDict.keys():
+          if self.state[key[0], key[1]] == (len(self.edgeDict[key]) + self.mineCount(key)):
+            for item in self.edgeDict[key]:
+              self.mineUpdate(item)
+
+      
+      def basicRule2(self):
+        selectCell = list()
+        for key in self.edgeDict.keys():
+          if self.state[key[0],key[1]] == self.mineCount(key):
+            selectCell.append(self.edgeDict[key])
+            return selectCell
+
+      def basicRule3(self):
+        
+          
+
+
+
+      # Count the marked mines around the coordinate
+      def mineCount(self, coordinate):
+        count = 0
+        for nb in self.getNeighbors(coordinate):
+          if self.state[nb[0],nb[1]] == MINE :
+            count += 1
+        return count
+                  
+          
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
