9f9607458ad29c95a1e7af75e6bdde83161c5640
jiahaobb <62066622+jiahaobb@users.noreply.github.com>
2021-01-06 22:32:57 +0800

    Update minesweeper_csp.py

--- code/minesweeper_csp.py
+++ code/minesweeper_csp.py
@@ -1,38 +1,31 @@
-from cspbase import *
 import itertools
 import numpy as np
+from cspbase import *
 from propagators import *
 import API
 
-def csp_model(minesweeper):
-    '''Initialize a csp model.
-    '''
 
+def csp_model(minesweeper):
     csp = CSP("Minesweeper")
 
-    # list of lists, same structure as board in minesweeper
     variables = []
-
-    # Initialize all variables in board.
     for row in range(minesweeper.dim1):
-        temp_row = []
+        row_vars = []
         for col in range(minesweeper.dim2):
-            name = str(row) + " " + str(col)
+            identity = str(row) + " " + str(col)
             if minesweeper.flag[row][col]:
                 domain = [1]
             elif not np.isnan(minesweeper.state[row][col]):
                 domain = [0]
             else:
-                domain = [0,1]
-            var = Variable(name, domain)
-            temp_row.append(var)
+                domain = [0, 1]
+            var = Variable(identity, domain)
+            row_vars.append(var)
             csp.add_var(var)
-        variables.append(temp_row)
+        variables.append(row_vars)
 
-    # Initialize all constraints.
-    # cons = [[name(str), [variable, variable,..], sum(int)], ...]
-    cons = []
-    unassign = []
+    constrains = []
+    unassigned = []
     for row in range(minesweeper.dim1):
         for col in range(minesweeper.dim2):
             if not np.isnan(minesweeper.state[row][col]):
@@ -40,76 +33,44 @@ def csp_model(minesweeper):
             elif minesweeper.flag[row][col]:
                 variables[row][col].assign(1)
             else:
-                unassign.append(variables[row][col])
+                unassigned.append(variables[row][col])
             if not np.isnan(minesweeper.state[row][col]) and not minesweeper.state[row][col] == 0:
-                surrounding = minesweeper.get_surrounding(row,col)
+                surrounding = minesweeper.get_surrounding(row, col)
                 scope = []
-                sum1 = minesweeper.state[row][col]
-                for x,y in surrounding:
+                mine_num = minesweeper.state[row][col]
+                for x, y in surrounding:
                     if minesweeper.flag[x][y]:
-                        sum1-=1
-                    if np.isnan(minesweeper.state[row][col]) and not minesweeper.flag[x][y]:
+                        mine_num -= 1
+                    if np.isnan(minesweeper.state[x][y]) and not minesweeper.flag[x][y]:
                         scope.append(variables[x][y])
-                name = str(row) + " " + str(col)
+                identity = str(row) + " " + str(col)
                 if scope:
-                    cons.append([name, scope, sum1])
-    
-    # for button in minesweeper.buttons:
-    #     # Assign value to all known variables.
-    #     if button.is_show():
-    #         variables[button.x][button.y].assign(0)
-    #     elif button.is_flag():
-    #         variables[button.x][button.y].assign(1)
-    #     else:
-    #         unassign.append(variables[button.x][button.y])
-    #     # Constraint info for a non-empty visible button.
-    #     if button.is_show() and not button.value == 0:
-    #         surrounding = minesweeper.get_surrounding_buttons(button.x, button.y)
-    #         scope = []
-    #         sum1 = button.value
-    #         for sur in surrounding:
-    #             if sur.is_flag():
-    #                 sum1 -= 1
-    #             if not sur.is_show() and not sur.is_flag():
-    #                 scope.append(variables[sur.x][sur.y])
-    #         name = str(button.x) + " " + str(button.y)
-    #         # Avoid empty scope. (All surrounding buttons are either visible or flagged.)
-    #         if scope:
-    #             cons.append([name, scope, sum1])
+                    constrains.append([identity, scope, mine_num])
 
-    # end-game: give it a fixed # 20:
-    # if len(unassign) <= 20:
-    #     cons.append(["endgame", unassign, minesweeper.remaining_mines])
+    constrains.sort(key=lambda c: len(c[1]))
 
-    # Sort cons by length of scope.
-    cons.sort(key=lambda x: len(x[1]))
-    # Reduce constraint's scope.
-    # ex: c1=[v1,v2,v3], c2=[v1,v2] => reduce c1 to [v3]
-    for i in range(len(cons)-1):
-        con1 = cons[i]
-        for j in range(i+1,len(cons)):
-            con2 = cons[j]
+    for i in range(len(constrains) - 1):
+        con1 = constrains[i]
+        for j in range(i+1, len(constrains)):
+            con2 = constrains[j]
             if set(con1[1]) == set(con2[1]):
                 continue
             if set(con1[1]) & set(con2[1]) == set(con1[1]):
                 con2[1] = list(set(con2[1]).difference(set(con1[1])))
                 con2[2] = con2[2] - con1[2]
 
-    # Sort cons by length of scope.
-    cons.sort(key=lambda x: len(x[1]))
-    # overlap cons, same structure as cons list: [[name(str), [variable, variable,..], sum(int)], ...]
+    constrains.sort(key=lambda c: len(c[1]))
     ol_cons = []
-    # list of lists, with [{var, var, var...},{}...]
     ol_set = []
     ol_var = []
 
     # Add new constraints if two constraints has at least two same variables in scope.
     # Create a new variable for overlap variables.
     # ex: c1=[v1,v2,v3], c2=[v2,v3,v4] => add c3=[v1,v2v3], c4=[v2v3,v4]. v2v3 is a new variable.
-    for i in range(len(cons)-1):
-        con1 = cons[i]
-        for j in range(i+1,len(cons)):
-            con2 = cons[j]
+    for i in range(len(constrains)-1):
+        con1 = constrains[i]
+        for j in range(i+1,len(constrains)):
+            con2 = constrains[j]
             if set(con1[1]) == set(con2[1]):
                 continue
             if 1 < len(set(con1[1]) & set(con2[1])):
@@ -118,13 +79,13 @@ def csp_model(minesweeper):
                 con2_vars = set(con2[1]) - ol_vars
                 con1_sum = con1[2]
                 con2_sum = con2[2]
-                name = ""
+                identity = ""
 
                 if not ol_vars in ol_set:
                     for i in ol_vars:
-                        name += i.name + ", "
-                    name = "(" + name + ")"
-                    var = Variable(name, list(range(len(ol_vars)+1)))
+                        identity += i.name + ", "
+                    identity = "(" + identity + ")"
+                    var = Variable(identity, list(range(len(ol_vars) + 1)))
                     csp.add_var(var)
                     ol_var.append(var)
                     ol_set.append(ol_vars)
@@ -137,10 +98,10 @@ def csp_model(minesweeper):
                 ol_cons.append(["", list(con1_vars), con1_sum])
                 ol_cons.append(["", list(con2_vars), con2_sum])
 
-    cons.extend(ol_cons)
+    constrains.extend(ol_cons)
 
     # Create Constraint object for constraint in cons list.
-    for con in cons:
+    for con in constrains:
         constraint = Constraint(con[0],con[1])
         tuples = satisfy_tuples(con[1],con[2])
         constraint.add_satisfying_tuples(tuples)
@@ -150,9 +111,6 @@ def csp_model(minesweeper):
 
 
 def satisfy_tuples(scope, sum1):
-    '''
-    '''
-
     product_list = []
     for var in scope:
         product_list.append(var.domain())
@@ -169,7 +127,7 @@ def solve_by_step(minesweeper):
     csp = csp_model(minesweeper)
 
     solver = BT(csp)
-    solver.bt_search_MS(prop_GAC)
+    solver.bt_search_MS(prop_FC)
     for var in csp.get_all_vars():
 
         try:
@@ -177,7 +135,7 @@ def solve_by_step(minesweeper):
             row = int(cell[0])
             col = int(cell[1])
         except:
-            # continue if it's not a vriable in board.
+            # continue if it's not a variable in board.
             # in board variable name's format: row, col
             continue
 
@@ -189,5 +147,3 @@ def solve_by_step(minesweeper):
             if np.isnan(minesweeper.state[row][col]):
                 return row,col
     return is_assigned
-
-
